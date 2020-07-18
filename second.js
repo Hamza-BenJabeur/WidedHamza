4c7409446ed86c041b3088d4008a78dd2f536c0e
Hamza-BenJabeur <Hamza.mhbj@gmail.com>
2020-07-18 11:09:55 +0200

    done

---
+++ second.js
@@ -0,0 +1,88 @@
+
+    var favorite=[];
+    var price={
+        espresso:2.5,
+        cappuccino:3.5,
+        latte:3.5,
+        coka:3.8,
+        fanta:3.8,
+        schweeeps:4,
+        strawberry:5.5,
+        wood:5.8,
+        banane:5,
+        caesar:18,
+        pulp:26,
+        green:13,
+        salmon:28,
+        cheese:25,
+        sea:35,
+        tuna:8,
+        veget:7.5,
+        pcheese:12,
+        bveg:12,
+        bcheese:15,
+        meat:18,
+        nutella1:7.5,
+        caramel1:6,
+        japanese:6,
+        nuttella2:7.5,
+        ferero:9,
+        caramel2:7,
+        fruitsalad:13
+    }
+    $(function() {
+       $("#price").click(function(){
+            $.each($("input[type='checkbox']:checked"), function(){
+                favorite.push($(this).val());
+                // console.log(favorite)
+                //var x=result();
+               
+
+                //localStorage.setItem("value",x);
+                // window.location.href="hbj.html";
+            });
+             $('#revenue').html(result()+'dt');
+
+           });
+       $("#but").click(function(e){
+        if (favorite.length>0){
+          $("#rev").html("you choose: "+favorite.join(","));
+      }
+      else{
+      alert("you didn't choose anything");}
+          e.preventDefault();
+      });
+       $('#another').on('click',function(){
+         location.reload(true);
+       });
+    });
+    function reset(){
+        favorite=[];
+         $('#reven ue').html(result()+'dt');
+
+    }
+    function result(){
+        var total=0;
+        for(var i=0;i<favorite.length;i++){
+            total=total+price[favorite[i]];
+        }
+        return total;
+    }
+
+
+  $("#btn1").on("click",function(){
+    $("#drinks").show();
+    $("#salty").hide();
+    $("#sweet").hide();
+  })
+
+$("#btn2").on("click",function(){
+    $("#salty").show();
+    $("#drinks").hide();
+    $("#sweet").hide();
+  })
+$("#btn3").on("click",function(){
+    $("#sweet").show();
+    $("#salty").hide();
+    $("#drinks").hide();
+  })
